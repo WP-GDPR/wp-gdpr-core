6764b46498a144628a61db1ab47ffb47d7f25fe4
Kim Vanrijckel <vanrijckel@gmail.com>
2018-06-22 16:22:05 +0200

    making function static

--- config/custom/text-config.php
+++ config/custom/text-config.php
@@ -5,31 +5,31 @@ namespace wp_gdpr\config\custom;
 class Text_Config {
 
 //Privacy Cneter page filters
-	public function filter_wp_comments() {
+	public static function filter_wp_comments() {
 		echo apply_filters( 'wp_gdpr_wp_comments', __( 'WordPress Comments', 'wp_gdpr' ) );
 	}
 
-	public function filter_wp_condolance_manager() {
+	public static function filter_wp_condolance_manager() {
 		echo apply_filters( 'wp_gdpr_wp_condolance_manager', __( 'Condolence Manager', 'wp_gdpr' ) );
 	}
 
-	public function filter_wp_cfdb7() {
+	public static function filter_wp_cfdb7() {
 		echo apply_filters( 'wp_gdpr_wp_cfdb7', __( 'Contact Form DB7', 'wp_gdpr' ) );
 	}
 
-	public function filter_wp_flamingo() {
+	public static function filter_wp_flamingo() {
 		echo apply_filters( 'wp_gdpr_wp_flamingo', __( 'Flamingo', 'wp_gdpr' ) );
 	}
 
-	public function filter_wp_mailchimp() {
+	public static function filter_wp_mailchimp() {
 		echo apply_filters( 'wp_gdpr_wp_mailchimp', __( 'Mailchimp', 'wp_gdpr' ) );
 	}
 
-	public function filter_wp_grafity_form() {
+	public static function filter_wp_grafity_form() {
 		echo apply_filters( 'wp_gdpr_wp_grafity_form', __( 'Gravity Forms', 'wp_gdpr' ) );
 	}
 
-	public function filter_wp_woocommerce() {
+	public static function filter_wp_woocommerce() {
 		echo apply_filters( 'wp_gdpr_wp_woocommerce', __( 'WooCommerce', 'wp_gdpr' ) );
 	}
 }
